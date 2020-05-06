6fb44cadecb46b2878f4a276418e67baefde3fb7
JakubRudniks <62304977+JakubRudniks@users.noreply.github.com>
2020-05-06 10:32:09 +0200

    Add files via upload

---
+++ Zadanie z jasona.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <fstream>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) 
+
+{
+	
+	ifstream wejscie;
+	ofstream wyjscie;
+	wyjscie.open("wej.txt");
+	wyjscie.open("wyj.txt");
+	
+	int tab[]={2, 3, 6, 7, 8, 7, 4, 3, 1, 7};
+	min=tab[0];
+	
+	if (wyjscie.good() )  
+	{
+		
+	while (!wejscie.eof() 	
+	for (int i=0; i<10; i++)
+		plik>>t[i];
+	}
+	
+	int  min=tab[0];
+		
+		
+	for(int i=0; i<10; i++) 
+		
+	{
+		
+	if(naj<t[i])
+	min=t[i]; 
+	
+	}	
+			cout<<"Najmniejsz¹ wczytan¹ liczb¹ jest"<<min<<endl;
+		
+			wyjscie<<" [  \n { \n "najmniejsza" : " " <<naj ;
+		
+			wejscie.clous();
+			wyjscie.clouse();
+			system("PAUSE");
+			
+			return 0;
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
